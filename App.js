e51c07b2a5313c2d4cc45c79f6ae78b2cf176209
Kira Slobodina <slobodina.kira@gmail.com>
2021-04-07 10:28:57 +0100

    Add messages and users

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const nodeFetch = require('node-fetch')
 const app = express()
 const port = 3001
 const whitelist = ['http://localhost:3000']
+let users
+const messages = []
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -17,25 +20,49 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-const message = {
-  id: 1,
-  content: 'Hello',
-  createdAt: '25.02.20',
-  author: 1,
-}
+// tell express to use json
+// https://nodejs.dev/learn/get-http-request-body-data-using-nodejs
+app.use(express.json())
+
+nodeFetch('https://randomuser.me/api/?results=2').then(res => res.json()).then(({ results }) => {
+  users = results.map(user => ({
+    id: user.login.uuid,
+    name: user.name.first,
+    avatar: user.picture.thumbnail,
+  }
+  ))
+  console.log('users', users)
+})
 
 const room = {
   id: 1,
   name: 'room1',
-  messages: [message],
+  messages,
   subscribedUsers: [],
 }
 
 app.get('/messages', (req, res) => {
-  return res.json(room)
+  setTimeout(() => {
+    return res.json(room)
+  }, 4000)
+})
+
+app.get('/user/:id', (req, res) => {
+  return res.json(users[+req.params.id])
+})
+
+app.post('/messages', (req, res) => {
+  if (!req.body.comment) {
+    res.status(400)
+    res.send('Comment missing')
+  } else {
+    const message = { content: req.body.comment, createdAt: new Date(), author: req.body.author }
+    messages.push(message)
+
+    return res.json(message)
+  }
 })
 
 app.listen(port, () =>
   console.log(`Chat app server listening on port ${port}`),
 );
-
